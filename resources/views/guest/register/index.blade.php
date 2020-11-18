40fbcac05043d5045b063d5385f0895212bd09ac
Lewis Hayter <Lewis15520@users.noreply.github.com>
2020-11-18 02:32:33 +0000

    front end for registration

--- resources/views/guest/register/index.blade.php
+++ resources/views/guest/register/index.blade.php
@@ -1,24 +1,56 @@
 @extends('templates.guest')
 @section('template_content')
 
-    @if(isset($errors))
-        @foreach($errors as $error)
-            @foreach($error as $message)
-                <p>{{ $message }}</p>
+    <div id="register">
+        @if(isset($errors))
+            @foreach($errors as $error)
+                @foreach($error as $message)
+                    <p>{{ $message }}</p>
+                @endforeach
             @endforeach
-        @endforeach
-    @endif
+        @endif
 
-    <form action="{{ route('register.post') }}" method="POST">
+        <form action="{{ route('register.post') }}" method="POST">
 
-        <input type="text" name="name" value="Lewis Hayter" required/>
-        <input type="email" name="email" value="lohayter@googlemail.com" required/>
-        <input type="password" name="password" value="password123" required/>
-        <input type="password" name="password_confirmation" value="password123" required/>
+            <div class="blockForm topLoader">
+                <div class="padding">
+                    <h1>Registration</h1>
+                </div>
+                <div class="padding">
+                    <p class="heading">Full Name</p>
+                    <p class="caption">Please type your full name here.</p>
+                    <input type="text" name="name" placeholder="E.g Joe Blogs" required/>
+                </div>
+                <div class="padding">
+                    <p class="heading">Email Address</p>
+                    <p class="caption">Please provide your email address.</p>
+                    <input type="email" name="email" placeholder="E.g joe.blogs@example.com" required/>
+                </div>
+                <div class="padding">
+                    <p class="heading">Password</p>
+                    <p class="caption">Type the password you would like to use for the account.</p>
+                    <input type="password" name="password" required/>
+                </div>
+                <div class="padding">
+                    <p class="heading">Confirm Password</p>
+                    <p class="caption">Please confirm the password.</p>
+                    <input type="password" name="password_confirmation" required/>
+                </div>
 
-        {{ csrf_field() }}
-        <button>Submit</button>
+                <div class="padding uiRow">
+                    <div class="uiCol --vertical-middle">
+                        <a href="#" class="dftButton --disabled">Back to login</a>
+                    </div>
+                    <div class="uiCol text-right --vertical-middle">
+                        <button class="dftButton --primary">Submit</button>
+                    </div>
+                </div>
 
-    </form>
+            </div>
+
+            {{ csrf_field() }}
+
+        </form>
+    </div>
 
 @endsection
